ea78541f385dfb68618c40fd3525ad7569dc05cd
Alvaro <celistony@yahoo.com>
2021-12-01 21:03:31 -0500

    inicio de prueba Bays

---
+++ app/Http/Controllers/RangosController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RangosController extends Controller
+{
+    //
+
+    // Metodo de prueba index................................................................
+
+    // Metodo de prueba store................................................................
+
+    //  Metodo de prueba update ..................................
+
+    //  Metodo de prueba destroy ..................................
+}
